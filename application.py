82c114cb7ebc4e64c037856a1a4e8a409a27d813
Nichita Kiselciuk <nichita.kiselciuk@gmail.com>
2021-02-25 20:46:14 +0200

    added azure ml model

--- application.py
+++ application.py
@@ -2,6 +2,8 @@ import os
 import numpy as np
 from flask import Flask,render_template,request
 import pickle
+import requests
+import json
 
 dirname = os.path.dirname(__file__)
 model_name = os.path.join(dirname, 'lr_model.pkl')
@@ -24,6 +26,22 @@ def predict_mean_value():
     output = round(prediction[0], 2)
 
     return render_template('index.html', prediction_text='Predicted Complex mean value $ {}'.format(output))
+
+@app.route('/predict_from_ml',methods=['POST'])
+def predict_mean_value():
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+
+    url = "http://ee627d30-5244-4116-83e4-ff8ccd8e8841.westeurope.azurecontainer.io/score"
+    data = {"data": final_features}
+    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+    r = requests.post(url, data=json.dumps(data), headers=headers)
+    data = r.json()
+    prediction = data['predict']
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_ml='Predicted Complex mean value $ {}'.format(output))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
