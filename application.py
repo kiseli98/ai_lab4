d9cba6a1e865fcc9c0acad0178f220ce8737b529
Nichita Kiselciuk <37908556+kiseli98@users.noreply.github.com>
2021-02-26 15:02:43 +0200

    revert application.py

--- application.py
+++ application.py
@@ -27,21 +27,6 @@ def predict_mean_value():
 
     return render_template('index.html', prediction_text='Predicted Complex mean value $ {}'.format(output))
 
-@app.route('/predict_from_ml',methods=['POST'])
-def predict_from_ml():
-    int_features = [int(x) for x in request.form.values()]
-    final_features = [np.array(int_features).tolist()]
-
-    url = "http://ee627d30-5244-4116-83e4-ff8ccd8e8841.westeurope.azurecontainer.io/score"
-    data = {"data": final_features}
-    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-    r = requests.post(url, data=json.dumps(data), headers=headers)
-    data = r.json()
-    prediction = data['predict']
-    output = round(prediction[0], 2)
-
-    return render_template('index.html', prediction_ml='Predicted Complex mean value $ {}'.format(output))
-
 
 if __name__ == "__main__":
     app.run(debug=True)
